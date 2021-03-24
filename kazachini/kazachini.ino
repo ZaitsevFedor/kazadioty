f7d773e6d0aa421c05157d0a1021ab81d595e1c4
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2021-03-24 18:13:47 +0500

    random

---
+++ kazachini/kazachini.ino
@@ -0,0 +1,26 @@
+void setup() {
+
+pinMode(1,1);
+pinMode(2,1);
+pinMode(3,1);
+pinMode(4,1);
+pinMode(6,0);
+pinMode(7,0);
+pinMode(8,0);
+pinMode(9,0);
+}
+a = 0;
+void loop(){
+digitalWrite(1,0);  
+digitalWrite(2,0);
+digitalWrite(3,0);
+digitalWrite(4,0);
+a = random(1,5);
+digitalWrite(a,1);
+while(1){
+  if digitalRead(6)
+}
+      
+
+
+}
